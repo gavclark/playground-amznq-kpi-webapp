43bcc668e98d0f6edd128da0a236359030fa56df
Gavin Clark <gav.clark@gmail.com>
2024-11-25 20:19:23 +0000

    updates

--- aws-exports.ts
+++ aws-exports.ts
@@ -1,23 +1,23 @@
 const awsmobile = {
-    "aws_project_region": "us-east-1",
-    "aws_cognito_region": "us-east-1",
+    "aws_project_region": "eu-west-1",
+    "aws_cognito_region": "eu-west-1",
     "aws_user_pools_id": "COGNITO-POOL-ID",
     "aws_user_pools_web_client_id": "COGNITO-CLIENT-ID",
     "aws_user_files_s3_bucket": "XXXXXXXXX",
-    "aws_user_files_s3_bucket_region": "us-east-1",
-    "aws_dynamodb_all_tables_region": "us-east-1",
+    "aws_user_files_s3_bucket_region": "eu-west-1",
+    "aws_dynamodb_all_tables_region": "eu-west-1",
     "aws_dynamodb_table_schemas": [
         {
             "tableName": "KPIs",
-            "region": "us-east-1"
+            "region": "eu-west-1"
         },
         {
             "tableName": "KPIValues",
-            "region": "us-east-1"
+            "region": "eu-west-1"
         },
         {
             "tableName": "KPIGroups",
-            "region": "us-east-1"
+            "region": "eu-west-1"
         }
     ]
 };
